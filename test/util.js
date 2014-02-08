d63f481f0fe640c25557d7fdc5249752558f57d2
Joey Zhou <josephyzhou@gmail.com>
2014-02-08 03:24:27 -0800

    stash

--- test/util.js
+++ test/util.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var Util = require('../lib/util');
+
 describe('Sha3 256 bits:', function() {
     var expectedHash = "7c5ea36004851c764c44143b1dcb59679b11c9a68e5f41497f6cf3d480715331";
     var sha3Hash = Util.sha3('HelloWorld');
