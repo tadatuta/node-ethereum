51644ced7a6272d8b5423ed7510ea87e4ddec0ed
Joey Zhou <josephyzhou@gmail.com>
2014-02-08 03:26:03 -0800

    stash

---
+++ test/db.js
@@ -0,0 +1,10 @@
+var DB = require('../lib/db');
+var Settings = require('../lib/settings').Settings;
+
+describe('LevelDB init:', function() {
+    it('should initialize without error',
+        function() {
+          config = new Settings();
+          console.log(config.homedir);
+        });
+});
