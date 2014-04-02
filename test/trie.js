e118bf61260e8ca379d1f0663deb569f28789dd6
Joey Zhou <josephyzhou@gmail.com>
2014-04-01 18:12:20 -0700

    update, get etc

--- test/trie.js
+++ test/trie.js
@@ -14,10 +14,18 @@ var fakeDB = {
     delete this.db[key];
   }
 };
+const LONG_VALUE = "1234567890abcdefghijklmnopqrstuvwxxzABCEFGHIJKLMNOPQRSTUVWXYZ";
 
 describe('Type checks:', function () {
   it('should type check trie.', function () {
     var n = new Trie(fakeDB, '', '');
     assert(Util.isTrie(n));
+  })
+});
+
+describe('Should update:', function () {
+  it('should update the trie with value provided.', function () {
+    var trie = new Trie(fakeDB, '', '');
+    trie.update('dog', LONG_VALUE);
   })
 });
