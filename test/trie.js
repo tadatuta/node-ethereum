f496cf7c243a6ec8768cb565a1b59b0571791241
Joey Zhou <josephyzhou@gmail.com>
2014-04-25 12:15:29 -0700

    finally got delete node in trie to work

--- test/trie.js
+++ test/trie.js
@@ -33,53 +33,53 @@ var fakeDB = {
 };
 const LONG_VALUE = "1234567890abcdefghijklmnopqrstuvwxxzABCEFGHIJKLMNOPQRSTUVWXYZ";
 
-// describe('Type checks:', function () {
-//   it('should type check trie.', function () {
-//     var n = new Trie(fakeDB, '', '');
-//     assert(Util.isTrie(n));
-//   });
-// });
+describe('Type checks:', function () {
+  it('should type check trie.', function () {
+    var n = new Trie(fakeDB, '', '');
+    assert(Util.isTrie(n));
+  });
+});
 
-// describe('should sync:', function () {
-//   afterEach(fakeDB.reset);
-//   var trie = new Trie(fakeDB, '', '');
-//   it('should sync and update the db accordingly.', function () {
-//     trie.update("dog", LONG_VALUE);
-//     assert(fakeDB.isEmpty());
-//     //sync
-//     trie.cache.commit();
-//     assert(!fakeDB.isEmpty());
-//   });
-// });
+describe('should sync:', function () {
+  afterEach(fakeDB.reset);
+  var trie = new Trie(fakeDB, '', '');
+  it('should sync and update the db accordingly.', function () {
+    trie.update("dog", LONG_VALUE);
+    assert(fakeDB.isEmpty());
+    //sync
+    trie.cache.commit();
+    assert(!fakeDB.isEmpty());
+  });
+});
 
-// describe('should undo:', function () {
-//   afterEach(fakeDB.reset);
-//   var trie = new Trie(fakeDB, '', '');
-//   it('should undo and update the db accordingly.', function () {
-//     trie.update("dog", LONG_VALUE);
-//     trie.cache.commit();
-//     var size = fakeDB.size();
-//     //adding something else
-//     trie.update("test", LONG_VALUE);
-//     trie.cache.undo();
-//     assert(size == fakeDB.size());
-//   });
-// });
+describe('should undo:', function () {
+  afterEach(fakeDB.reset);
+  var trie = new Trie(fakeDB, '', '');
+  it('should undo and update the db accordingly.', function () {
+    trie.update("dog", LONG_VALUE);
+    trie.cache.commit();
+    var size = fakeDB.size();
+    //adding something else
+    trie.update("test", LONG_VALUE);
+    trie.cache.undo();
+    assert(size == fakeDB.size());
+  });
+});
 
-// describe('should cache nodes:', function () {
-//   after(fakeDB.reset);
-//   var trie = new Trie(fakeDB, '', '');
-//   it('cache nodes size should increase when we update trie.', function () {
-//     trie.update("dog", LONG_VALUE);
-//     var size = Util.size(trie.cache.nodes);
-//     assert(size > 0);
-//   });
-//   it('cache nodes size should stay the same when we undo cache', function () {
-//     trie.cache.undo();
-//     var size = Util.size(trie.cache.nodes);
-//     assert(size === 0);
-//   });
-// });
+describe('should cache nodes:', function () {
+  after(fakeDB.reset);
+  var trie = new Trie(fakeDB, '', '');
+  it('cache nodes size should increase when we update trie.', function () {
+    trie.update("dog", LONG_VALUE);
+    var size = Util.size(trie.cache.nodes);
+    assert(size > 0);
+  });
+  it('cache nodes size should stay the same when we undo cache', function () {
+    trie.cache.undo();
+    var size = Util.size(trie.cache.nodes);
+    assert(size === 0);
+  });
+});
 
 describe('Should be able to update and get trie:', function () {
   afterEach(fakeDB.reset);
@@ -91,48 +91,32 @@ describe('Should be able to update and get trie:', function () {
     assert(Util.deepEqual(result, LONG_VALUE));
   });
 
-  // it('should work for official universal trie test #1', function () {
-  //   var trie = new Trie(fakeDB, '', '');
-  //   var exp = 'd85f9267d7ed5767fb1b48defa8eb20a1c007a87a07588135a74b40b55de2e67';
-  //   trie.update('doe', 'reindeer');
-  //   trie.update('dog', 'puppy');
-  //   trie.update('dogglesworth', 'cat');
-  //   console.log('official trie test #1 root:' + trie.root);
-  //   assert(Util.deepEqual(exp, trie.root));
-  // });
-
-  // it('should work for official universal trie test #2', function () {
-  //   var trie = new Trie(fakeDB, '', '');
-  //   var exp = 'cf7d318935b52db6e23d8c1f5e6b7e62f3606d4ed13783f4fdbd6e67a2085d04';
-  //   trie.update('do': 'verb');
-  //   trie.update('horse', 'stallion');
-  //   trie.update('doge', 'coin');
-  //   trie.update('dog', 'puppy');
-  //   console.log('official trie test #2 root:' + trie.root);
-  //   assert(Util.deepEqual(exp, trie.root));
-  // });
-
-  // it('should do replace when update with same key', function () {
-  //   var trie = new Trie(fakeDB, '', '');
-  //   trie.update('dog', LONG_VALUE);
-  //   var result = trie.get('dog');
-  //   trie.update('dog', LONG_VALUE + 'yolo');
-  //   result = trie.get('dog');
-  //   assert(Util.deepEqual(result, LONG_VALUE + 'yolo'));
-  // });
+  it('should do replace when update with same key', function () {
+    var trie = new Trie(fakeDB, '', '');
+    trie.update('dog', LONG_VALUE);
+    var result = trie.get('dog');
+    trie.update('dog', LONG_VALUE + 'yolo');
+    result = trie.get('dog');
+    assert(Util.deepEqual(result, LONG_VALUE + 'yolo'));
+  });
 });
 
-// describe('Should be able to delete nodes from trie:', function () {
-//   afterEach(fakeDB.reset);
+describe('Should be able to delete nodes from trie:', function () {
+  afterEach(fakeDB.reset);
 
-//   it('should be able to delete node.', function () {
-//     var trie = new Trie(fakeDB, '', '');
-//     trie.update('cat', LONG_VALUE);
-//     var expected = trie.root;
-//     console.log('expected: ', expected);
-//     trie.update('dog', LONG_VALUE);
-//     trie.del('dog');
-//     console.log('actual: ', trie.root);
-//     assert(Util.deepEqual(expected, trie.root));
-//   });
-// });
+  it('should be able to delete node.', function () {
+    var trie = new Trie(fakeDB, '', '');
+    trie.update('cat', LONG_VALUE);
+    var expected = trie.root;
+    trie.get('cat');
+    trie.update('dog', LONG_VALUE);
+    var postDog = trie.root;
+    // trie.get('dog');
+    trie.del('dog');
+    console.log('expected: ', expected);
+    console.log('after dog: ', postDog);
+    console.log('actual: ', trie.root);
+
+    assert(Util.deepEqual(expected, trie.root));
+  });
+});
