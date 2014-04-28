3a802e0472602c53df5dfab43704f94019500450
wanderer <mbecze@craveprogramming.com>
2014-04-28 19:22:36 -0400

    save block to db

--- bin/ethereum.js
+++ bin/ethereum.js
@@ -3,17 +3,21 @@
 var argv = require('minimist')(process.argv.slice(2));
 
 var Ethereum = require('../'),
+    async = require('async'),
     Network = Ethereum.Network,
     DB = Ethereum.DB,
+    db = DB.db,
     internals = {};
 
-internals.network = new Network({publicIp: argv["public-ip"]});
-
-internals.network.on('connecting', function(socket, port, host){
-    console.log(host + ":"+port + ' connecting');
+internals.network = new Network({
+    publicIp: argv["public-ip"]
 });
 
-internals.network.on('closing', function(peer){
+internals.network.on('connecting', function(socket, port, host) {
+    console.log(host + ":" + port + ' connecting');
+});
+
+internals.network.on('closing', function(peer) {
     console.log(peer.internalId + " closing");
 });
 
@@ -31,7 +35,7 @@ internals.network.on('message.transactions', function(transactions, peer) {
 
 internals.network.on('message.peers', function(peers, peer) {
     console.log(peers);
-    console.log( peer.internalId + ' got peers');
+    console.log(peer.internalId + ' got peers');
 });
 
 internals.network.on('message.getPeers', function(peers, peer) {
@@ -39,15 +43,60 @@ internals.network.on('message.getPeers', function(peers, peer) {
 });
 
 internals.network.on('message.blocks', function(blocks, peer) {
-
-    for (var i=0; i < blocks.length; i++) {
-        //db.put("block:" + block.header.number);
+    for (var i = 0; i < blocks.length; i++) {
+        var block = blocks[i];
+        //TODO: if valid then save
+        db.put(block.hash(), block.rlpSerialize());
     }
     console.log(peer.internalId + ' got blocks');
-    //TODO: check if block is in DB
 });
 
 internals.network.on('message.getChain', function(message, peer) {
+    var i = 0,
+        foundParent = false;
+
+    //find the parent
+    async.whilst(
+        function() {
+            return !foundParent && i < message.parents.length
+        },
+        function() {
+            db.get(message.parents[i], function(err, value) {
+                i++;
+                if (!err) {
+                    foundParent = value;
+                }
+            });
+        },
+        function(err) {}
+    );
+
+    if (foundParent) {
+        var blockFounds = [],
+            moreBlock = true;
+
+        //find the children
+        /*
+        async.whilst(
+            function() {
+                return blockFounds.length < message.count && moreBlock;
+            },
+            function() {
+                db.get(found, function(err, value) {
+                    i++;
+                    if (!err) {
+                        foundParent = value;
+                    }
+                });
+            },
+            function(err) {}
+        );
+        */
+        //internals.network.blocks();
+        
+    } else {
+        internals.network.sendNotInChain();
+    }
     console.log(peer.internalId + " got get chain");
 });
 
