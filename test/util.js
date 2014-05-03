72292d9b615bf4e4c7d42e0d98903a76b8fe3f63
Joey Zhou <josephyzhou@gmail.com>
2014-05-03 14:35:23 -0700

    correct order of assert for util

--- test/util.js
+++ test/util.js
@@ -21,18 +21,18 @@ describe('Compact encoding:', function () {
   it('should handle odd case', function () {
     var testBuffer = [0, 1, 2, 3, 4, 5];
     var res = Util.compactEncode(testBuffer).toJSON();
-    assert.deepEqual([0, 1, 35, 69], res);
+    assert.deepEqual(res, [0, 1, 35, 69]);
   });
   it('should handle odd case with terminator', function () {
     var test = [6, 4, 6, 15, 6, 7, 16];
     var res = Util.compactEncode(test).toJSON();
-    assert.deepEqual([32, 100, 111, 103], res);
+    assert.deepEqual(res, [32, 100, 111, 103]);
   });
 
   it('should handle even case with terminator', function () {
     var test = [15, 1, 12, 11, 8, 16];
     var res = Util.compactEncode(test).toJSON();
-    assert.deepEqual([63, 28, 184], res);
+    assert.deepEqual(res, [63, 28, 184]);
   });
 
 });
@@ -41,7 +41,7 @@ describe('Compact hex decoding:', function () {
   it('should decode to hex.', function () {
     var exp = [7, 6, 6, 5, 7, 2, 6, 2, 16];
     var res = Util.compactHexDecode('verb');
-    assert.deepEqual(exp, res);
+    assert.deepEqual(res, exp);
   });
 
 });
@@ -51,27 +51,27 @@ describe('Compact decoding:', function () {
     var testBuffer = new Buffer([17, 35, 69]);
     var exp = [1, 2, 3, 4, 5];
     var res = Util.compactDecode(testBuffer);
-    assert.deepEqual(exp, res);
+    assert.deepEqual(res, exp);
   });
 
   it('should handle odd case', function () {
     var testBuffer = new Buffer([0, 1, 35, 69]);
     var exp = [0, 1, 2, 3, 4, 5];
     var res = Util.compactDecode(testBuffer);
-    assert.deepEqual(exp, res);
+    assert.deepEqual(res, exp);
   });
 
   it('should handle odd case with terminator', function () {
     var testBuffer = new Buffer([32, 15, 28, 184]);
     var exp = [0, 15, 1, 12, 11, 8, 16];
     var res = Util.compactDecode(testBuffer);
-    assert.deepEqual(exp, res);
+    assert.deepEqual(res, exp);
   });
   it('should handle even case with terminator', function () {
     var testBuffer = new Buffer([63, 28, 184]);
     var exp = [15, 1, 12, 11, 8, 16];
     var res = Util.compactDecode(testBuffer);
-    assert.deepEqual(exp, res);
+    assert.deepEqual(res, exp);
   });
 
 });
