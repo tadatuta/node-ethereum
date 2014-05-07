f090604bbbc0b4fdcf68eb7ad4d1d1c3bdc4671a
Joey Zhou <josephyzhou@gmail.com>
2014-05-07 15:24:02 -0700

    changing dependencies

--- test/trie.js
+++ test/trie.js
@@ -1,4 +1,4 @@
-var Trie = require('../lib/schema/trie/trie');
+var Trie = require('../lib/trie/trie');
 var Util = require('../lib/util');
 var assert = require('assert');
 
@@ -115,13 +115,13 @@ describe('Should be able to delete nodes from trie:', function () {
 });
 
 describe.skip('Testing root hash', function (argument) {
-  it('should match the python implementation', function() {
+  it('should match the python implementation', function () {
     var trie = new Trie(fakeDB, '', '');
-    trie.update('dog','puppy');
-    trie.update('horse','stallion');
+    trie.update('dog', 'puppy');
+    trie.update('horse', 'stallion');
     trie.update('do', 'verb');
-    trie.update('doge','coin');
-    
+    trie.update('doge', 'coin');
+
     assert.equal(trie.root, '5991bb8c6514148a29db676a14ac506cd2cd5775ace63c30a4fe457715e9ac84');
   });
 });
